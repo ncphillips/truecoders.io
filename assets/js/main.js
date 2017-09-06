b3d24c008e0e9ba36f00e0c98eec46fc388bc8bc
Cody Winton <cody@credntia.com>
2017-09-05 19:30:32 -0500

    Added video intro via. Popup

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,11 @@
 * Zoom Images, Get Date and Shadow
 * ========================================================================== */
 
-(function() {
-
-})();
+$(document).ready(function() {
+  //$('.subsection-image').magnificPopup({type:'image'});
+  //$('.display-2').css("color", "red");
+  $('.video-player').magnificPopup({
+    type: 'iframe'
+    // other options
+  });
+});
