423fdb219dd1d3b10b2e776c722bc50551708f52
Cody Winton <cody.t.winton@gmail.com>
2017-12-12 20:03:48 -0600

    Transistioned skills-bar-chart over to javascript

--- _includes/skills/skills-bar-chart.js
+++ _includes/skills/skills-bar-chart.js
@@ -1,52 +1,52 @@
+Chart.defaults.global.defaultFontFamily = 'Montserrat';
 
+var skills = {{ include.skills | jsonify }};
+var skillsElement = document.getElementById("skills-chart");
 
-  Chart.defaults.global.defaultFontFamily = 'Montserrat';
+if (skillsElement && skills.length > 1) {
+  var labels = [];
+  var levels = [];
+  var backgroundColors = [];
+  var borderColors = [];
 
-  var skills = {{ include.skills | jsonify }};
-  var skillsElement = document.getElementById("skills-chart");
+  for (var i = 0; i < skills.length; i++) {
+    var item = skills[i];
 
-  if (skillsElement && skills.length > 1) {
-    var labels = [];
-    var levels = [];
-    var backgroundColors = [];
-    var borderColors = [];
-
-    for (var i = 0; i < skills.length; i++) {
-      var item = skills[i];
-
-      if (item.level == null || item.level <= 1) { continue }
-      
-      labels.push(item.name);
-      levels.push(item.level);
-      backgroundColors.push('rgba(0, 147, 201, 1)');
-      borderColors.push('rgba(0, 147, 201, 1)');
+    if (item.level == null || item.level <= 1) {
+      continue
     }
 
-    var skillsChart = new Chart(skillsElement.getContext('2d'), {
-      type: 'horizontalBar',
-      data: {
-        labels: labels,
-        datasets: [{
-          label: 'Skills',
-          data: levels,
-          backgroundColor: backgroundColors,
-          borderColor: borderColors,
-          borderWidth: 1
-        }]
-      },
-      options: {
-        legend: {
-          display: false
-        },
-        scales: {
-          xAxes: [{
-            ticks: {
-              max: 5,
-              min: 1,
-              stepSize: 1
-            }
-          }]
-        }
-      }
-    });
+    labels.push(item.name);
+    levels.push(item.level);
+    backgroundColors.push('rgba(0, 147, 201, 1)');
+    borderColors.push('rgba(0, 147, 201, 1)');
   }
+
+  var skillsChart = new Chart(skillsElement.getContext('2d'), {
+    type: 'horizontalBar',
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Skills',
+        data: levels,
+        backgroundColor: backgroundColors,
+        borderColor: borderColors,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      legend: {
+        display: false
+      },
+      scales: {
+        xAxes: [{
+          ticks: {
+            max: 5,
+            min: 1,
+            stepSize: 1
+          }
+        }]
+      }
+    }
+  });
+}
