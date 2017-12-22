5b536405c7ca162d8b43c87d1788ad0ed9ebaa61
Cody Winton <cody.t.winton@gmail.com>
2017-12-22 11:52:57 -0800

    Updated remote theme to 0.9.7

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  //$('.subsection-image').magnificPopup({type:'image'});
-  //$('.display-2').css("color", "red");
-  $('.video-player').magnificPopup({
-    type: 'iframe'
-    // other options
-  });
-});
